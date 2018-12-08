5aeb70b15a0722ec02b0a0074c7ad0eb5b36e967
Dan Kolbman <dankolbman@gmail.com>
2018-12-08 12:40:46 -0500

    :sparkles: Add trip model

--- detour/api/models.py
+++ detour/api/models.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Trip(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Point(models.Model):
     id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
 
     time = models.DateTimeField(auto_now_add=True)
     lat = models.FloatField()
