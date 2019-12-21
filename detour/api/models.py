468c7adc217e04763b66f1194980c40cfd26b509
Dan Kolbman <dankolbman@gmail.com>
2019-12-21 11:22:51 -0500

    :card_file_box: Add visible field

--- detour/api/models.py
+++ detour/api/models.py
@@ -8,6 +8,7 @@ class Trip(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
+    visible = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name}'
