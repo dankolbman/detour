95865a12745d8539963d2bf95f1f6d704a58bba0
Dan Kolbman <dankolbman@gmail.com>
2019-12-21 10:54:47 -0500

    :sparkles: Add geoJson field

--- detour/graphql/trips.py
+++ detour/graphql/trips.py
@@ -1,8 +1,18 @@
-from graphene import relay, ObjectType
+from graphene import relay, ObjectType, Field, List, String, Int
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from detour.api.models import Trip
+
+
+class Geometry(ObjectType):
+    type = String(default_value="LineString")
+    coordinates = List(List(Int))
+
+
+class GeoJSON(ObjectType):
+    type = String(default_value="Feature")
+    geometry = Field(Geometry)
 
 
 class TripNode(DjangoObjectType):
@@ -10,6 +20,16 @@ class TripNode(DjangoObjectType):
         model = Trip
         filter_fields = ["name"]
         interfaces = (relay.Node,)
+
+    geoJSON = Field(GeoJSON)
+
+    def resolve_geoJSON(self, info, **kwargs):
+
+        coordinates = self.points.values("lat", "lon").order_by("time").all()
+
+        return GeoJSON(
+            geometry=Geometry(coordinates=coordinates.values_list("lon", "lat"))
+        )
 
 
 class Query(ObjectType):
